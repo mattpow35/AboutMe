b8d5b5c5698517212e0b50782b48cc457603dbf4
Matt Powley <mattpow35@gmail.com>
2016-10-03 09:26:32 -0600

    made home screen with link to second screen with hobbies on it

---
+++ AboutMe/Controller/SecondScreen.swift
@@ -0,0 +1,17 @@
+//
+//  SecondScreen.swift
+//  AboutMe
+//
+//  Created by Powley, Matthew on 10/3/16.
+//  Copyright © 2016 Powley, Matthew. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreen : UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
