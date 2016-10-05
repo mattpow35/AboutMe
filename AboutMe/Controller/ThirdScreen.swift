0da95ce041f72e634d8ff3b81f92fa270fcae6f9
Matt Powley <mattpow35@gmail.com>
2016-10-05 08:23:48 -0600

    started third screen about favorite things

---
+++ AboutMe/Controller/ThirdScreen.swift
@@ -0,0 +1,15 @@
+//
+//  ThirdScreen.swift
+//  AboutMe
+//
+//  Created by Powley, Matthew on 10/5/16.
+//  Copyright © 2016 Powley, Matthew. All rights reserved.
+//
+
+import UIKit
+
+class ThirdScreen : UIViewController
+{
+    
+}
+ 
