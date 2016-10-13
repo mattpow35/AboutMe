34d43599da74399cd9170fe673026f67296ced82
Matt Powley <mattpow35@gmail.com>
2016-10-13 08:12:15 -0600

    finished screen on social media

--- AboutMe/FourthScreen.swift
+++ AboutMe/FourthScreen.swift
@@ -10,5 +10,33 @@ import UIKit
 
 class FourthScreen : UIViewController
 {
+    @IBOutlet private weak var instaImage: UIImageView!
+    @IBOutlet private weak var snapImage: UIImageView!
+    @IBOutlet private weak var twitterImage: UIImageView!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func instaButton(sender: AnyObject)
+    {
+        instaImage.image = UIImage(named: "FullSizeRender 4")
+    }
+    
+    
+    @IBAction func snapButton(sender: AnyObject)
+    {
+        snapImage.image = UIImage(named: "FullSizeRender 6")
+    }
+    
+    @IBAction func twitterButton(sender: AnyObject)
+    {
+        twitterImage.image = UIImage(named: "FullSizeRender 5")
+    }
+    
+    
+    
     
 }
